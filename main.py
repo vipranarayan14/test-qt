229aad762b5a19ff30bb52386eaf15b8d3b68262
Prasanna Venkatesh <vipranarayan14@gmail.com>
2020-01-07 08:57:10 +0530

    learn to insert mainwindow and label

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.setWindowTitle('Learning Qt with PyQt')
+
+        label = QLabel('Hi, this is my first Qt app!')
+        label.setAlignment(Qt.AlignCenter)
+        self.setCentralWidget(label)
 
 app = QApplication([])
 
-window = QWidget()
+window = MainWindow()
 window.show()
 
-app.exec() # exec() = exec_() in PyQt5
+app.exec()  # exec() = exec_() in PyQt5
