6f6121fe75416e2cbbb9664780a9f82627e42adc
Prasanna Venkatesh <vipranarayan14@gmail.com>
2020-01-07 14:58:15 +0530

    try event handling. add 'docstring' (docs in python) to avoid pylint warning. format constants to uppercase to avoid pylint warning.

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
+'''Python bindings for qt5.'''
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
+    '''Subclass extending QMainWindow for customization.'''
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
+        self.windowTitleChanged.connect(self.on_window_title_change)
         self.setWindowTitle('Learning Qt with PyQt')
 
         label = QLabel('Hi, this is my first Qt app!')
         label.setAlignment(Qt.AlignCenter)
         self.setCentralWidget(label)
 
-app = QApplication([])
+    # have to use this "decorater" if 'self' or 'cls' is not a arg for the a class method
+    @staticmethod
+    def on_window_title_change(event):
+        '''Prints the new title.'''
+        print(event)
 
-window = MainWindow()
-window.show()
+APP = QApplication([])
 
-app.exec()  # exec() = exec_() in PyQt5
+WINDOW = MainWindow()
+
+WINDOW.show()
+
+APP.exec()  # exec() = exec_() in PyQt5
